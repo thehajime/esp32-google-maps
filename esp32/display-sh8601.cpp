thehajime/esp32-google-maps

commit 6e964f75f91cba17b0607efd685988636341489c

opposites the light theme switch

esp32/theme.h
@@ -22,7 +22,7 @@ namespace ThemeControl {
 				return;
 
 			isLightHardware = value;
-			value ? lcd.invertDisplay(true) : lcd.invertDisplay(false);
+			value ? lcd.invertDisplay(false) : lcd.invertDisplay(true);
 		}
 
 		bool isHardwareLight() {

commit 10ad2526686633e8645c2af1f0828f562694c919

fix flashScreen issue of keep reverting after flash
Fixes: 6e964f7 ("opposites the light theme switch")
Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

esp32/display-sh8601.cpp
@@ -268,7 +268,8 @@ void SimpleSh8601::flushWindow(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y
 }
 
 void SimpleSh8601::invertDisplay(bool invert) {
-	uint32_t lcd_cmd = invert ? LCD_CMD_INVON : LCD_CMD_INVOFF;
+	/* ???: opposite ? */
+	uint32_t lcd_cmd = invert ? LCD_CMD_INVOFF : LCD_CMD_INVON;
 
 	lcd_cmd &= 0xff;
 	lcd_cmd <<= 8;

esp32/esp32.ino
@@ -181,7 +181,7 @@ void loop() {
 	}
 
 	if (DUMMY_SPEED_DATA) {
-		Pref::speedLimit = 90;
+		Pref::speedLimit = 50;
 		DO_EVERY(500) {
 			int speed = 40 + 40*sin((millis() / 1000));
 			navigationQueue.push(String("speed=") + speed);

esp32/theme.h
@@ -20,7 +20,7 @@ namespace ThemeControl {
 				return;
 
 			isLightHardware = value;
-			value ? lcd->invertDisplay(false) : lcd->invertDisplay(true);
+			value ? lcd->invertDisplay(true) : lcd->invertDisplay(false);
 		}
 
 		bool isHardwareLight() {