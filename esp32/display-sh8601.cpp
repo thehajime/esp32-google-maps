thehajime/esp32-google-maps

commit 6e964f75f91cba17b0607efd685988636341489c

opposites the light theme switch

esp32/theme.h
@@ -22,7 +22,7 @@ namespace ThemeControl {
 				return;
 
 			isLightHardware = value;
-			value ? lcd.invertDisplay(true) : lcd.invertDisplay(false);
+			value ? lcd.invertDisplay(false) : lcd.invertDisplay(true);
 		}
 
 		bool isHardwareLight() {

commit a101d552ff0820282953e4219603eab7d9328726

fix again for light theme switch
Fixes: 6e964f7 ("opposites the light theme switch")
Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

esp32/display-sh8601.cpp
@@ -268,9 +268,7 @@ void SimpleSh8601::flushWindow(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y
 }
 
 void SimpleSh8601::invertDisplay(bool invert) {
-	/* ???: opposite ? */
-	uint32_t lcd_cmd = invert ? LCD_CMD_INVOFF : LCD_CMD_INVON;
-
+	uint32_t lcd_cmd = invert ? LCD_CMD_INVON : LCD_CMD_INVOFF;
 	lcd_cmd &= 0xff;
 	lcd_cmd <<= 8;
 	lcd_cmd |= LCD_OPCODE_WRITE_CMD << 24;

esp32/esp32.ino
@@ -136,7 +136,7 @@ void setup() {
 	Data::init();
 
 	lcd->setBrightness(Pref::brightness);
-	ThemeControl::dark();
+	ThemeControl::light();
 
 	/* init battery */
 	initBatery();

esp32/preferences.h
@@ -2,7 +2,7 @@
 #define PREFERENCES_H
 
 namespace Pref {
-	bool lightTheme = false;
+	bool lightTheme = true;
 	int brightness = 60;
 	int speedLimit = 60;
 } // namespace Pref

esp32/theme.h
@@ -11,8 +11,8 @@ namespace ThemeControl {
 		uint32_t lastFlashRequest_ms  = 0;
 		uint32_t offWithTimerStart_ms = 0;
 		int8_t toggleCount            = -1;
-		bool isLight                  = false;
-		bool isLightHardware          = false;
+		bool isLight                  = true;
+		bool isLightHardware          = true;
 		bool isWaitingForDark         = false;
 
 		void writeLight(bool value) {
@@ -20,7 +20,8 @@ namespace ThemeControl {
 				return;
 
 			isLightHardware = value;
-			value ? lcd->invertDisplay(true) : lcd->invertDisplay(false);
+			/* ???: opposite ? */
+			value ? lcd->invertDisplay(false) : lcd->invertDisplay(true);
 		}
 
 		bool isHardwareLight() {