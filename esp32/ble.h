f41cd206764a704597925aae7c4a4f04d1ee0b13
Hajime Tazaki <thehajime@gmail.com>
2026-06-22 12:34:19 +0900

    Revert "add ble advert"
    This reverts commit 8cf59f23013b744acffc4627fbeb8e0290729f3a.
    
    it is too early for human to use UI programs with location-based
    notification....

--- esp32/ble.h
+++ esp32/ble.h
@@ -8,7 +8,6 @@
 #include <BLESecurity.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLEBeacon.h>
 #include <vector>
 
 BLEServer* pServer      = NULL;
@@ -175,28 +174,7 @@ void initBle() {
 		serviceConfig.bleService->start();
 	}
 
-	BLEAdvertising *pAdvertising = server.bleServer->getAdvertising();
-
-	/* iBeacon */
-	BLEBeacon oBeacon = BLEBeacon();
-	oBeacon.setManufacturerId(0x4C00); // Apple's iBeacon manufacturer ID
-	oBeacon.setProximityUUID(BLEUUID(catDriveService.uuid));
-	oBeacon.setMajor(1);
-	oBeacon.setMinor(1);
-
-	BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
-	BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
-
-	//oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED
-	String serviceData = "";
-	serviceData += (char)0x1A; // Length
-	serviceData += (char)0xFF; // Company ID
-	serviceData += oBeacon.getData();
-
-	oAdvertisementData.addData(serviceData);
-	pAdvertising->setAdvertisementData(oAdvertisementData);
-	pAdvertising->setScanResponseData(oScanResponseData);
-	pAdvertising->start();
+	server.bleServer->getAdvertising()->start();
 }
 
 void notifyCharacteristic(const String& uuid, uint8_t* data, size_t length) {
